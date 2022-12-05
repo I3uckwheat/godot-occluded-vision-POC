3d79b939d9d1fb03a1b6e8c7c4bebe6fd7ec74d9
Briggs Elsperger <briggse@protonmail.com>
2022-12-04 21:36:27 -0700

    FOV and aiming

---
+++ light-test/Player.gd
@@ -0,0 +1,49 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var aiming_speed = 180
+export var normal_speed = 350
+#var screen_size
+#@export var set_shadows: bool = false : set = enable_shadow
+
+func enable_shadow(val):
+#	$RedLight.shadow_enabled = val
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		if Input.is_action_pressed("ready_weapon"):
+			velocity = velocity.normalized() * aiming_speed
+			look_at(get_global_mouse_position())
+		else:
+			velocity = velocity.normalized() * normal_speed
+			
+	
+	if Input.is_action_just_pressed("ready_weapon"):
+		get_node("CollisionShape2D/Weapon").visible = true
+	elif Input.is_action_just_released("ready_weapon"):
+		get_node("CollisionShape2D/Weapon").visible = false
+
+	position += velocity * delta
+	
+	
+	
