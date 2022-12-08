ec653b71753c9e742269b78a33a32485912c7e5a
Briggs Elsperger <briggse@protonmail.com>
2022-12-07 22:51:05 -0700

    Change to physics based movement

--- light-test/Player.gd
+++ light-test/Player.gd
@@ -1,14 +1,7 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-export var aiming_speed = 180
-export var normal_speed = 350
-#var screen_size
-#@export var set_shadows: bool = false : set = enable_shadow
+export var aiming_speed = 1.4
+export var normal_speed = 2.8
 
 func enable_shadow(val):
 #	$RedLight.shadow_enabled = val
@@ -16,35 +9,34 @@ func enable_shadow(val):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$PlayerFOV.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO
+
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x = 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x = -1
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		velocity.y = 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		
-	if velocity.length() > 0:
-		if Input.is_action_pressed("ready_weapon"):
-			velocity = velocity.normalized() * aiming_speed
-		else:
-			velocity = velocity.normalized() * normal_speed
+		velocity.y = -1
 	
 	if Input.is_action_pressed("ready_weapon"):
-		look_at(get_global_mouse_position())
-	
+		$Body.look_at(get_global_mouse_position())
+#		Just create a child to rotate instead of this RigidBody2d
+
 	if Input.is_action_just_pressed("ready_weapon"):
-		get_node("Weapon").visible = true
+		$Body/PlayerBody/Weapon.visible = true
 	elif Input.is_action_just_released("ready_weapon"):
-		get_node("Weapon").visible = false
+		$Body/PlayerBody/Weapon.visible = false
 	
-	move_and_slide(velocity)
+	var speed = normal_speed
+	if Input.is_action_pressed("ready_weapon"):
+		speed = aiming_speed
 	
-	
-	
+	apply_central_impulse(velocity.normalized() * speed)
+
+# Use linear_velocity for facing while walking normally (not aiming)
