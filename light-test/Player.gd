f8f2ab87388137c8fac48095378bba447e0f18c2
Briggs Elsperger <briggse@protonmail.com>
2022-12-11 12:35:00 -0700

    Upgrade player movement

--- light-test/Player.gd
+++ light-test/Player.gd
@@ -3,30 +3,28 @@ extends RigidBody2D
 export var aiming_speed = 1.4
 export var normal_speed = 2.8
 
-func enable_shadow(val):
-#	$RedLight.shadow_enabled = val
-	pass
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PlayerFOV.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
+func _process(_delta):
+	var move_impulse = Vector2.ZERO
 
 	if Input.is_action_pressed("move_right"):
-		velocity.x = 1
+		move_impulse.x = 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x = -1
+		move_impulse.x = -1
 	if Input.is_action_pressed("move_down"):
-		velocity.y = 1
+		move_impulse.y = 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y = -1
+		move_impulse.y = -1
 	
 	if Input.is_action_pressed("ready_weapon"):
+		#	Just create a child to rotate instead of this RigidBody2d
 		$Body.look_at(get_global_mouse_position())
-#		Just create a child to rotate instead of this RigidBody2d
+	else:
+		$Body.rotation = self.linear_velocity.angle()
 
 	if Input.is_action_just_pressed("ready_weapon"):
 		$Body/PlayerBody/Weapon.visible = true
@@ -37,6 +35,6 @@ func _process(delta):
 	if Input.is_action_pressed("ready_weapon"):
 		speed = aiming_speed
 	
-	apply_central_impulse(velocity.normalized() * speed)
+	apply_central_impulse(move_impulse.normalized() * speed)
 
 # Use linear_velocity for facing while walking normally (not aiming)
